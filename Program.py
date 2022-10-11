6016a0230aba28f1f76f58e5b085c1355edbddc7
Skateliv <115539693+Skateliv@users.noreply.github.com>
2022-10-11 14:57:04 +0530

    Update Program.py
    Added line to the python file to have the statemental series of the function of given program.

--- Program.py
+++ Program.py
@@ -1,14 +1,20 @@
 #include<stdio.h>    
 int main()
-{    
- int n1=0,n2=1,n3,i,number;    
- printf("Enter the number of elements:");    
+{  
+
+ int n1=0,n2=1,n3,i,number:
+ printf("------Program fabonacci Series------");
+
+
+ printf("Enter the number of elements:"); 
+ //this helps in the functioning of creating series printing the series
  scanf("%d",&number);    
  printf("\n%d %d",n1,n2); 
  for(i=2;i<number;++i)  
  {    
   n3=n1+n2;    
-  printf(" %d",n3);    
+  printf(" %d",n3);  
+  //program to have the operation technique of functioning the setup
   n1=n2;    
   n2=n3;    
  }  
